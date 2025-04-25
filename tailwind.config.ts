a4ba023840bd5679abb6c6ee71f615bc12904740
Nimrod Kibet <nimrodmandelakibet@gmail.com>
2025-04-25 16:58:14 +0200

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,16 @@ import { heroui } from '@heroui/react';
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ['./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}'],
-  theme: {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],  theme: {
     extend: {
       screens: {
         // Custom 4K screen — kicks in at 2560px and above
+        '3xl': '1440px',
         '4k': '2560px',
       },
     },
