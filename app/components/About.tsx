ceee69277fa1c88eada9958789f35439c25a9135
Nimrod Kibet <nimrodmandelakibet@gmail.com>
2025-05-04 11:15:15 +0200

    about tablet responsiveness

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -2,23 +2,28 @@
 
 export function About() {
   return (
-    <section className="relative bg-[#F8EDE5] py-16">
-      <div className="flex flex-col md:flex-row-reverse items-center">
+    <section className="relative bg-[#F8EDE5] md:py-[50px]  ">
+      <div className="flex flex-col md:flex-row-reverse items-center ">
         {/* Right Image */}
-        <div className="w-full md:w-1/2 h-[600px]">
+        <div className="w-full md:w-1/2  ">
           <img
-            src="/about-image.svg" // your image here
+            src="/about-image.svg"
             alt="Hat and Notebook"
-            className="object-cover w-full h-full"
+            className="object-cover w-full md:h-[500px] lg:h-[720px]"
           />
         </div>
 
         {/* Left White Card */}
-        <div className="w-full md:w-1/2 flex justify-center">
-          <div className="bg-white shadow-lg p-8 md:p-12 max-w-lg relative -mt-12 md:mt-0 md:-mr-20 z-10">
+        <div className=" md:w-1/2   flex justify-center">
+          <div
+            className="bg-white text-[14px] lg:text-[16px] lg:leading-[32px]  lg:w-[650px] shadow-lg
+          md:px-[20px] lg:px-[40px] md:py-[10px] lg:py-[20px] relative md:absolute md:right-[40%] lg:right-[605px] md:top-1/2 md:-translate-y-1/2 md:ml-[50px]  z-10"
+          >
             {/* About Me */}
-            <h3 className="text-md font-bold mb-4">ABOUT ME - EMMA NASHIPAE</h3>
-            <p className="text-sm text-gray-700 leading-relaxed mb-8">
+            <h3 className="text-[14px] lg:text-[18px] font-medium ">
+              ABOUT ME - EMMA ONDITI
+            </h3>
+            <p className="my-[5px] lg:my-[10px]">
               I’m Emma Onditi, originally from Kenya, dedicated to helping{' '}
               <b>Immigrants</b> transition smoothly to <b>Austria</b> and{' '}
               <b>Europe</b>. With 13 years of personal experience navigating
@@ -29,31 +34,36 @@ export function About() {
             </p>
 
             {/* Core Values */}
-            <h4 className="text-md font-bold mb-4">CORE VALUES</h4>
+            <h4 className="text-[14px] lg:text-[18px] font-medium mb-[5px] lg:mb-[10px]">
+              CORE VALUES
+            </h4>
 
-            <ul className="space-y-4 text-sm text-gray-700">
-              <li className="border-b border-[#DDDDDD] pb-2">
-                <b>R.I.S.E</b> - Resilience, Inspiration, Self-awareness,
-                Empowerment
+            <ul className="space-y-[5px] lg:space-y-[10px]">
+              <li className="border-b font-normal border-[#DDDDDD] ">
+                <b className="font-medium">R.I.S.E</b> - Resilience,
+                Inspiration, Self-awareness, Empowerment
               </li>
-              <li className="border-b border-[#DDDDDD] pb-2">
-                <b>S.O.A.R</b> - Self-discovery, Optimism, Action-taking,
-                Resilience
+              <li className="border-b font-normal border-[#DDDDDD]">
+                <b className="font-medium">S.O.A.R</b> - Self-discovery,
+                Optimism, Action-taking, Resilience
               </li>
-              <li className="border-b border-[#DDDDDD] pb-2">
-                <b>E.A.C.H</b> - Thoughts Empower Action and Clarity
+              <li className="border-b font-normal border-[#DDDDDD]">
+                <b className="font-medium">E.A.C.H</b> - Thoughts Empower Action
+                and Clarity
               </li>
-              <li className="border-b border-[#DDDDDD] pb-2">
-                <b>T.F.R.A</b> - Take Full Responsibility for Actions
+              <li className="border-b font-normal border-[#DDDDDD]">
+                <b className="font-medium">T.F.R.A</b> - Take Full
+                Responsibility for Actions
               </li>
-              <li className="border-b border-[#DDDDDD] pb-2">
-                <b>L.I.V.E</b> - Learn, Invest, Value, Embrace
+              <li className="border-b font-normal border-[#DDDDDD]">
+                <b className="font-medium">L.I.V.E</b> - Learn, Invest, Value,
+                Embrace
               </li>
             </ul>
 
             {/* CTA Button */}
-            <div className="mt-8">
-              <button className="bg-[#7B3939] text-white py-3 px-6 uppercase text-sm font-semibold hover:bg-[#592828] transition rounded-sm">
+            <div className="md:mt-[10px] lg:mt-[22px] ">
+              <button className="bg-[#7B3939] text-white  md:px-3 md:py-2  lg:py-3 lg:px-6 uppercase md:text-[12px] lg:text-[16px]font-semibold hover:bg-[#592828] transition ">
                 Book a Free Session
               </button>
             </div>
